7a2b48b116b95faf56b7276f895988bb405db673
3bdalazeez <191021@ppu.edu.ps>
2023-02-21 13:48:51 +0200

    separate routes

---
+++ routes/index.js
@@ -0,0 +1,4 @@
+export { default as article } from './article.js';
+export { default as magazine } from './magazine.js';
+export { default as user } from './user.js';
+export { default as favorite } from './favourite.js';
