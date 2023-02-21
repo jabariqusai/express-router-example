cb61505709ec21de1245e6aa8d40d0add073a827
Momen-G-Ar <cp.momen@gmail.com>
2023-02-21 13:52:37 +0200

    add index.js in routers folder to export routers

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from 'express';
-import articleRouter from './routers/article.js';
-import userRouter from './routers/user.js';
-import magazineRouter from './routers/magazine.js';
-import favoriteRouter from './routers/favorite.js';
+import { article, favorite, magazine, user } from './routers/index.js';
 const PORT = 3001;
 
 const app = express();
-app.use('/article', articleRouter);
-app.use('/user', userRouter);
-app.use('/magazine', magazineRouter);
-app.use('/favorite', favoriteRouter);
-
+app.use('/article', article);
+app.use('/user', user);
+app.use('/magazine', magazine);
+app.use('/favorite', favorite);
 
 app.listen(PORT, () => console.debug('Server is listening on port', PORT));
