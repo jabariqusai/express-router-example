177a6534835a675309cb3078f83f568d44b77bac
raghad-salem <raghadnadersalem20022017@gmail.com>
2023-02-21 13:33:47 +0200

    split code functionality into routers

---
+++ routers/favorite.js
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+
+const router = Router() ;
+
+/**
+ * Retrieve an favorite by id
+ */
+router.get('/favorite/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Get favorite ${id}`);
+});
+
+/**
+ * Retrieve a list of favorites
+ */
+router.get('/favorite', (req, res) => {
+  res.send(`List favorites`);
+});
+
+/**
+ * Create a new favorite
+ */
+router.post('/favorite', (req, res) => {
+  res.send('Create favorite');
+});
+
+/**
+ * Update an existing favorite
+ */
+router.post('/favorite/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Update favorite ${id}`);
+});
+
+/**
+ * Delete an favorite by id
+ */
+router.post('/favorite/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Delete favorite ${id}`);
+});
+
+export default router ;
