a7415dccff0981a630ddd521bb08286e4edff079
Lana <lanaqawasmy7@gmail.com>
2023-02-21 13:52:47 +0200

    Define separate routers and put the prefix outside the router

---
+++ routes/article.js
@@ -0,0 +1,44 @@
+import { Router } from 'express';
+
+const router = Router();
+
+
+/**
+ * Retrieve an article by id
+ */
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Get article ${id}`);
+});
+
+/**
+ * Retrieve a list of articles
+ */
+router.get('/', (req, res) => {
+  res.send(`List articles`);
+});
+
+/**
+ * Create a new article
+ */
+router.post('/', (req, res) => {
+  res.send('Create article');
+});
+
+/**
+ * Update an existing article
+ */
+router.post('/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Update article ${id}`);
+});
+
+/**
+ * Delete an article by id
+ */
+router.post('/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Delete article ${id}`);
+});
+
+export default router;
---
+++ routes/user.js
@@ -0,0 +1,45 @@
+import { Router } from 'express';
+
+const router = Router();
+
+
+/**
+ * Retrieve an user by id
+ */
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Get user ${id}`);
+});
+
+/**
+ * Retrieve a list of users
+ */
+router.get('/', (req, res) => {
+  res.send(`List users`);
+});
+
+/**
+ * Create a new user
+ */
+router.post('/', (req, res) => {
+  res.send('Create user');
+});
+
+/**
+ * Update an existing user
+ */
+router.post('/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Update user ${id}`);
+});
+
+/**
+ * Delete an user by id
+ */
+router.post('/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Delete user ${id}`);
+});
+
+
+export default router;
