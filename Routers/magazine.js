07ae1d70c8bed0ff6a2791a63a9d1f435d93f3db
dala_bilal <dala_bilal@gmail.com>
2023-02-21 13:40:58 +0200

    adding a router for each endpoint

---
+++ Routers/magazine.js
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+
+const router = Router();
+
+/**
+ * Retrieve an magazine by id
+ */
+router.get('/magazine/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Get magazine ${id}`);
+});
+
+/**
+ * Retrieve a list of magazines
+ */
+router.get('/magazine', (req, res) => {
+  res.send(`List magazines`);
+});
+
+/**
+ * Create a new magazine
+ */
+router.post('/magazine', (req, res) => {
+  res.send('Create magazine');
+});
+
+/**
+ * Update an existing magazine
+ */
+router.post('/magazine/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Update magazine ${id}`);
+});
+
+/**
+ * Delete an magazine by id
+ */
+router.post('/magazine/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Delete magazine ${id}`);
+});
+
+export default router;
