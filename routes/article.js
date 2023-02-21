577873be9721de93ebcbb8c440abf9f43589ce50
abdullah-nuaimi <181004@ppu.edu.ps>
2023-02-21 13:35:19 +0200

    split app to specific routes

---
+++ routes/article.js
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+
+const router = Router();
+
+/**
+ * Retrieve an article by id
+ */
+router.get('/article/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Get article ${id}`);
+});
+
+/**
+ * Retrieve a list of articles
+ */
+router.get('/article', (req, res) => {
+  res.send(`List articles`);
+});
+
+/**
+ * Create a new article
+ */
+router.post('/article', (req, res) => {
+  res.send('Create article');
+});
+
+/**
+ * Update an existing article
+ */
+router.post('/article/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Update article ${id}`);
+});
+
+/**
+ * Delete an article by id
+ */
+router.post('/article/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Delete article ${id}`);
+});
+
+export default router;
