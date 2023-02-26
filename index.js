8c68b059cef66225729f904ee4398184cf635d73
Khaldoun halawani <khalawani@sadasol.com>
2023-02-26 11:50:58 +0200

    Add logging middleware globally

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import { logger } from './middlewares/index.js';
 import { articleRouter, magazineRouter, userRouter, favoriteRouter } from './routes/index.js';
 
 const PORT = 3001;
 
 const app = express();
 
+app.use(logger);
 app.use('/article', articleRouter);
 app.use('/magazine', magazineRouter);
 app.use('/user', userRouter);
