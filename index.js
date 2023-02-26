fbb031b4a8aa5f229f064e6bf14139139cc5c2fc
Lana <lanaqawasmy7@gmail.com>
2023-02-26 12:09:51 +0200

    Create a middleware called endpointLogger

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ import articleRouter from './routes/article.js';
 import magazineRouter from './routes/magazine.js';
 import userRouter from './routes/user.js';
 import favoriteRouter from './routes/favorite.js';
+import endpointLogger from './middleware/index.js';
+
 
 const PORT = 3001;
 
 const app = express();
 
-app.use('/article',articleRouter);
-app.use('/magazine',magazineRouter);
-app.use('/user',userRouter);
-app.use('/favorite' ,favoriteRouter);
+app.use(endpointLogger);
+
+app.use('/article', articleRouter);
+app.use('/magazine', magazineRouter);
+app.use('/user', userRouter);
+app.use('/favorite', favoriteRouter);
 
 
 app.listen(PORT, () => console.debug('Server is listening on port', PORT));
