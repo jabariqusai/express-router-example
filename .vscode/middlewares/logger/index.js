6cd2487ff2cc7ff6dba37e672a008f3cdd31def3
ruba <ruba@gmail.com>
2023-02-26 13:48:25 +0200

    middleware

---
+++ .vscode/middlewares/logger/index.js
@@ -0,0 +1,6 @@
+const logger = (req, res, next) =>{
+  console.info(`${req.method} ${req.originalUrl} ${new Date().toISOString()}`);
+  next();
+
+}
+export default logger
---
+++ .vscode/middlewares/time-logger/index.js
@@ -0,0 +1,14 @@
+const timeLogger = (req, res, next) =>{
+ if (res.locals.reqStartTime) {
+const reqFirstDate = res.locals.reqStartDate;
+const reqFinalDate = new Date();
+console.info(`Elapsed time: ${reqFinalDate - reqFirstDate}ms`);
+return next();
+ }
+ res.locals.reqStartTime = new Date()
+ next();
+ console.log('Test')
+
+};
+
+export default timeLogger
