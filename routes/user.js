e32fe98d8a47a3eb75679d843705129ed49a4e1b
abdullah-nuaimi <181004@ppu.edu.ps>
2023-02-28 12:37:56 +0200

    add jsonwebtoken pkg with simple ex

--- routes/user.js
+++ routes/user.js
@@ -1,43 +1,41 @@
 import { Router } from 'express';
-
+import jwt from 'jsonwebtoken';
 const router = Router();
+
+const passwords = {
+  qjabari: '1234',
+  mnajar: '4321'
+};
+
+const users = [
+  {
+    username: 'qjabari',
+    firstName: 'Qusai',
+    lastName: 'Jabari',
+    email: 'qjabari@sadasol.com'
+  },
+  {
+    username: 'mnajar',
+    firstName: 'Moayed',
+    lastName: 'Najar',
+    email: 'mnajar@sadasol.com'
+  }
+];
 
 /**
  * Retrieve an user by id
  */
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  res.send(`Get user ${id}`);
+router.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const user = users.find((user) => user.username == username);
+  if(!user || passwords[username] != password){
+    res.status(400).send('username or password incorrect');
+  }else{
+    const token = jwt.sign(user, 'why', {subject:'why'});
+    res.send(token);
+  }
 });
 
-/**
- * Retrieve a list of articles
- */
-router.get('/', (req, res) => {
-  res.send(`List users`);
-});
-
-/**
- * Create a new user
- */
-router.post('/', (req, res) => {
-  res.send('Create user');
-});
-
-/**
- * Update an existing user
- */
-router.post('/:id', (req, res) => {
-  const id = req.params.id;
-  res.send(`Update user ${id}`);
-});
-
-/**
- * Delete an user by id
- */
-router.post('/:id', (req, res) => {
-  const id = req.params.id;
-  res.send(`Delete user ${id}`);
-});
 
 export default router;
