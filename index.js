4c10fd24c3845a6f4522c9c60648ad6f7ec1da0b
jabariqusai <qjabari@sadasol.com>
2023-03-02 13:24:32 +0200

    Role-based access checking

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { logger, errorHandling, guard } from './middleware/index.js';
-import { articleRouter, magazineRouter, userRouter, favoriteRouter } from './routes/index.js';
+import { articleRouter, magazineRouter, userRouter, favoriteRouter, adminRouter } from './routes/index.js';
 
 const PORT = 3001;
 
@@ -12,6 +12,8 @@ app.use(logger);
 app.use('/article', errorHandling);
 app.use('/magazine', magazineRouter);
 app.use('/user', userRouter);
-app.use('/favorite', guard, favoriteRouter);
+app.use('/favorite', guard(), favoriteRouter);
+// Only admin users should access the admin router
+app.use('/admin', guard('admin'), adminRouter);
 
 app.listen(PORT, () => console.debug('Server is listening on port', PORT));
---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/self-destruct', (req, res) => {
+  res.send('Initiating self-destruction');
+});
+
+router.get('/fire-rockets', (req, res) => {
+  res.send('Firing rockets');
+});
+
+router.get('/delete-database', (req, res) => {
+  res.send('Database deleted');
+});
+
+export default router;
